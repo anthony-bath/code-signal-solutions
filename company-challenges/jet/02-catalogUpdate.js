694649cc407149686aa18a981d4940e5ac26efeb
Anthony Bath <anthony@crushingit.io>
2019-01-25 18:29:36 -0500

    completed solution to the catalogUpdate Jet company challenge

--- company-challenges/jet/02-catalogUpdate.js
+++ company-challenges/jet/02-catalogUpdate.js
@@ -1,10 +1,8 @@
-//Incomplete - Passing 6/7
-
 const catalogUpdate = (catalog, updates) => {
   const catalogMap = new Map();
 
   catalog.forEach(([key, ...items]) => {
-    catalogMap.set(key, [...items]);
+    catalogMap.set(key, items);
   });
 
   updates.forEach(([key, ...items]) => {
@@ -13,7 +11,7 @@ const catalogUpdate = (catalog, updates) => {
     if (catalogMap.has(key)) {
       updated = [...catalogMap.get(key), ...items];
     } else {
-      updated = [...items];
+      updated = items;
     }
 
     catalogMap.set(key, updated);
@@ -22,23 +20,8 @@ const catalogUpdate = (catalog, updates) => {
   const result = [];
 
   for (let [key, value] of catalogMap) {
-    result.push([key, ...[...value].sort(comparer)]);
+    result.push([key, ...value.sort()]);
   }
 
-  return result.sort((a, b) => {
-    //moves the'root' entry to the end
-    if (a[0] === 'root') return 1;
-    if (b[0] === 'root') return -1;
-
-    return comparer(a[0], b[0]);
-  });
-};
-
-const comparer = (a, b) => {
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] < b[i]) return -1;
-    if (a[i] > b[i]) return 1;
-  }
-
-  return a > b;
+  return result.sort((a, b) => a[0] > b[0]);
 };
